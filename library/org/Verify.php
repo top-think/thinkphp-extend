75853e0562d783fe026072403bfaff6715d6977f
thinkphp <thinkphp@qq.com>
2016-02-17 17:35:20 +0800

    修正Verify类

--- library/org/Verify.php
+++ library/org/Verify.php
@@ -130,7 +130,7 @@ class Verify
         // 验证码字体随机颜色
         $this->_color = imagecolorallocate($this->_image, mt_rand(1, 150), mt_rand(1, 150), mt_rand(1, 150));
         // 验证码使用随机字体
-        $ttfPath = dirname(__FILE__) . '/Verify/' . ($this->useZh ? 'zhttfs' : 'ttfs') . '/';
+        $ttfPath = dirname(__FILE__) . '/verify/' . ($this->useZh ? 'zhttfs' : 'ttfs') . '/';
 
         if (empty($this->fontttf)) {
             $dir  = dir($ttfPath);
@@ -274,7 +274,7 @@ class Verify
      */
     private function _background()
     {
-        $path = dirname(__FILE__) . '/Verify/bgs/';
+        $path = dirname(__FILE__) . '/verify/bgs/';
         $dir  = dir($path);
 
         $bgs = [];
