dc897df245319e46a8b8c87f5b291e419485aeb5
zzpuser <zzpuser@163.com>
2016-06-17 10:25:47 +0800

    将NOW_TIME修改为time()
    5.0中已没有NOW_TIME常量

--- extend/org/Verify.php
+++ extend/org/Verify.php
@@ -96,7 +96,7 @@ class Verify
             return false;
         }
         // session 过期
-        if (NOW_TIME - $secode['verify_time'] > $this->expire) {
+        if (time() - $secode['verify_time'] > $this->expire) {
             session($key, null);
             return false;
         }
@@ -180,7 +180,7 @@ class Verify
         $code                  = $this->authcode(strtoupper(implode('', $code)));
         $secode                = [];
         $secode['verify_code'] = $code; // 把校验码保存到session
-        $secode['verify_time'] = NOW_TIME; // 验证码创建时间
+        $secode['verify_time'] = time(); // 验证码创建时间
         session($key . $id, $secode);
 
         header('Cache-Control: private, max-age=0, no-store, no-cache, must-revalidate');
